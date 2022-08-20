34130b606947999a899149dd62540357bca6194a
Nicolae Ciobanu <cnicolae2000@gmail.com>
2022-08-20 20:15:49 +0300

    build: add components export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from "./Greeting/Greeting";
 export * from "./Logo/Logo";
 export * from "./Network/Network";
 export * from "./Skills/Skills";
+export * from "./Footer/Footer";
+export * from "./NotFound/NotFound";
