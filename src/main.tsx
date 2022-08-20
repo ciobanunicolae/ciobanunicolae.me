74f89744a2b7a249dabd444de335ef196a93a97b
Nicolae Ciobanu <cnicolae2000@gmail.com>
2022-08-20 20:14:17 +0300

    refactor: move react-router to root

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
+import { Footer, NotFound } from "./components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
