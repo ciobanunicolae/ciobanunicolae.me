b1dd33ac189c5d6ce339df149e412a8e97b8bd44
Nicolae Ciobanu <cnicolae2000@gmail.com>
2022-08-21 14:53:09 +0300

    styled: add media queries

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,8 @@
 import styled from "./Footer.module.css";
-import { FiMail } from "react-icons/fi";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 export const Footer = () => (
   <>
     <div className={styled.footer}>
-      <div className={styled.links}>
-        <a href="mailto:ciobanu.nicolae.j@gmail.com">
-          <FiMail />
-        </a>
-        <a href="https://www.linkedin.com/in/ciobanunicolae/">
-          <FaLinkedin />
-        </a>
-        <a href="https://github.com/ciobanunicolae">
-          <FaGithub />
-        </a>
-      </div>
       <div className={styled.copyright}>© Ciobanu Nicolae 2022</div>
     </div>
   </>
