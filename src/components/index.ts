e6b53c4f464f17efd0608b5e1ebf269f8ddd582d
nicu <ciobanu.nicolae.j@gmail.com>
2022-09-18 15:30:30 -0700

    fix:

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,4 @@ export * from "./Logo/Logo";
 export * from "./Network/Network";
 export * from "./Skills/Skills";
 export * from "./Navbar/Navbar";
-export * from "./Projects/Projects";
+export * from "./projects/Projects";
