b2074a8b3451b5131a877e264b57585627b9e2a2
Ciobanu Nicolae <ciobanu.nicolae.j@gmail.com>
2022-09-18 13:28:49 -0700

    Delete src/components/Greeting directory

--- src/components/Greeting/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "./greeting.module.css";
-
-export const Greeting = () => (
-  <>
-    <h1 className={styled.text} id='about'>
-      I'm Ciobanu Nicolae, a web developer based in Sibiu, Romania.
-    </h1>
-  </>
-);
