c41c7fe107eae9393bc9cfab88feaaaaa0ef9bba
nicu <ciobanu.nicolae.j@gmail.com>
2022-09-18 15:39:19 -0700

    refactor: change components folders name

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,8 +3,8 @@ import styled from "./Projects.module.css";
 export const Projects = () => {
   return (
     <div className={styled.container}>
-      <div className={styled.project}>Work in progress</div>
-      <div className={styled.project}>Work in progress</div>
+      <div className={styled.project} />
+      <div className={styled.project} />
     </div>
   );
 };
