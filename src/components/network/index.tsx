944ac1b28ebe2bd9bb36a2d9ae12d58f4e16fee1
nicu <ciobanu.nicolae.j@gmail.com>
2022-09-19 23:09:22 -0700

    chore: fix scroll break

--- src/components/network/index.tsx
+++ src/components/network/index.tsx
@@ -2,11 +2,17 @@ import styled from "./network.module.css";
 
 export const Network = () => (
   <>
-    <div className={styled.network} id='network'>
+    <div className={styled.network} id="network">
       <span className={styled.category}>NETWORK</span>
-      <a href="mailto:ciobanu.nicolae.j@gmail.com">Mail</a>
-      <a href="https://www.linkedin.com/in/ciobanunicolae/">LinkedIn</a>
-      <a href="https://github.com/ciobanunicolae">GitHub</a>
+      <a target={"_blank"} href="mailto:ciobanu.nicolae.j@gmail.com">
+        Mail
+      </a>
+      <a target={"_blank"} href="https://www.linkedin.com/in/ciobanunicolae/">
+        LinkedIn
+      </a>
+      <a target={"_blank"} href="https://github.com/ciobanunicolae">
+        GitHub
+      </a>
     </div>
   </>
 );
